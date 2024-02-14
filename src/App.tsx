e328cdefbdbc8719cea1eb596ebd3114b8eed1d2
AbiR7171 <abiremtiag@gmail.com>
2024-02-14 16:07:53 +0600

    project setup completed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <div>
+           <Button className='bg-red-600'>Ready</Button>
+     </div>
+  )
+}
+
+export default App
